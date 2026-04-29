ac0d9a24f953a5eaf0cb8dac75af40a7c1cd3861
Gopal71 <gopalkpuranik@gmail.com>
2026-04-29 23:43:12 +0530

    fixed a function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
+app.ge('/', (req, res) => {
 res.send('Hello CI/CD World!');
 });
 app.listen(3001, () => console.log('Server running'));
