917695a2859a6807b0c4b44d834e3f7d4936d044
Gopal71 <gopalkpuranik@gmail.com>
2026-04-30 14:00:28 +0530

    added a test to validate - content should have hello ci/cd world

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -17,5 +17,10 @@ test('homepage should respond with status 200', async ({ page }) => {
     const response = await page.goto('http://localhost:3001');
     expect(response.status()).toBe(200);
 }   );  
+test('homepage should contain "Hello CI/CD World!"', async ({ page }) => {
+    await page.goto('http://localhost:3001');
+    const content = await page.textContent('body');
+    expect(content).toContain('Hello CI/CD World!');
+});
 
 
