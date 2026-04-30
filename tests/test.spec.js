ae560d1b83dcefed5fe44d06876e60adcf803d55
Gopal71 <gopalkpuranik@gmail.com>
2026-04-30 13:01:48 +0530

    added a test to validate the body content

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -3,4 +3,10 @@ import { test, expect } from '@playwright/test';
 test('homepage should load', async ({ page }) => {
     await page.goto('http://localhost:3001');
 });
+test('homepage should display correct message', async ({ page }) => {
+    await page.goto('http://localhost:3001');
+    const content = await page.textContent('body');
+    expect(content).toBe('Hello CI/CD World!');
+});
+
 
