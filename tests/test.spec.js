d2855bb6db370025672fac6140aa20db9b2c85c3
Gopal71 <gopalkpuranik@gmail.com>
2026-05-04 16:41:12 +0530

    corrected test

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -6,7 +6,7 @@ test('homepage should load', async ({ page }) => {
 test('homepage should display correct message', async ({ page }) => {
     await page.goto('http://localhost:3001');
     const content = await page.textContent('body');
-    expect(content).toBe('Hello CI/CD Word!');
+    expect(content).toBe('Hello CI/CD World!');
 });
 test('homepage should have correct title', async ({ page }) => {
     await page.goto('http://localhost:3001');
